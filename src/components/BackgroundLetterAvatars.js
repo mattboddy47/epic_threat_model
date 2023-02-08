2e3684da721341de4a487425a602d0ffc55f7465
matt <mattboddy47@gmail.com>
2023-02-08 10:26:54 +0000

    removed string to color for avatars

--- src/components/BackgroundLetterAvatars.js
+++ src/components/BackgroundLetterAvatars.js
@@ -3,26 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import { deepPurple } from '@mui/material/colors';
 
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
 function stringAvatar(name) {
   return {
     sx: {
